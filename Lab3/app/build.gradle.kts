7dbc0febc4d520c101cf44edc946f4f6906d4a5e
Zhanibek Tulegen <https://github.com/TulegenZhanibek/Web-Dev>
2024-05-16 20:14:04 +0600

    Add

--- Lab3/app/build.gradle.kts
+++ Lab3/app/build.gradle.kts
@@ -91,9 +91,9 @@ dependencies {
     implementation("androidx.room:room-common:2.6.1")
 
     // Testing Dependencies
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("com.makeramen:roundedimageview:2.3.0")
     implementation("androidx.core:core-ktx:1.9.0")
--- Lab3/app/src/main/java/com/example/lab3/database/DbConstant.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lab3.database
-
-object DbConstant {
-    const val DB_NAME = "Project"
-    const val USER_TABLE = "Users"
-}
